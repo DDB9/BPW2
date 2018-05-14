716c9749c52b4762166a51fae716323fb8bbe455
Daan de Bruijn <ddebruijn9@gmail.com>
2018-05-14 13:13:27 +0200

    Added Unity Project

---
+++ BuildingPlayfullWorlds2/Assets/gameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+
+	public GameObject platform;
+
+	void OnTriggerEnter(Collider other){
+		Instantiate(platform, platform.transform.position + Vector3.forward * 12, platform.transform.rotation);
+	}
+}
