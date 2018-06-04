c1ba78d1b57e3be04ad42e8bd2623b1e7e9f6658
Daan de Bruijn <ddebruijn9@gmail.com>
2018-06-04 12:29:55 +0200

    Started with player jumping and edited a little of the forward movement. Player no longer tumbles as the move forward.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float movSpeed = 10;
+	public float jumpForce = 5;
+	public Rigidbody player;
+	// Practice force on the gameobject to make it move.
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.velocity = new Vector3(0,0,movSpeed);
+
+		if (Input.GetMouseButtonDown(0)){
+			player.AddForce(new Vector3(0,1,0) * jumpForce * Time.deltaTime, ForceMode.Impulse);
+		}
+	}
+}
