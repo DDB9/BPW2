cd130c582ec76ee93ce60bd95a171387ac3dbb9e
Daan de Bruijn <ddebruijn9@gmail.com>
2018-06-18 21:55:26 +0200

    Plop! BLOOOOB. Added sounds for when the ads appear on screen. Sounds shift in pitch so they sound different each time.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -9,6 +9,7 @@ public class gameManager : MonoBehaviour {
 	public Rigidbody player;
 	public GameObject platform;
 	public Canvas playerScreen;
+	public AudioClip adPlop;
 
 	public GameObject adProductPosOne;
 	public GameObject adProductPosTwo;
@@ -18,8 +19,11 @@ public class gameManager : MonoBehaviour {
 	private List<GameObject> adList = new List<GameObject>(2);
 	public static bool adActive = false;
 	private GameObject spawnedAd;
+	private AudioSource source;
 
 	void Start(){
+		source = GetComponent<AudioSource>();
+
 		adList.Add(adProductPosOne);
 		adList.Add(adProductPosTwo);
 		adList.Add(adVirusPosOne);
@@ -31,6 +35,9 @@ public class gameManager : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
+		source.PlayOneShot(adPlop, 1f);
+		source.pitch = Random.Range(0.5f, 1.0f);
+
 		Instantiate(platform, 
 					platform.transform.position + Vector3.forward * 12, 
 					platform.transform.rotation);
